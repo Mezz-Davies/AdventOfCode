2f313e248c0d62f0f0ecb40984dedfbb3fe02b41
MezzOMG <mkgdavies@gmail.com>
2020-12-09 20:43:20 +0000

    added encoding to readFileSync

--- templates/JS/main.js
+++ templates/JS/main.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 const path = require('path');
-const fileContents = fs.readFileSync(path.resolve(__dirname, 'input.txt'));
+const fileContents = fs.readFileSync(path.resolve(__dirname, 'input.txt'), {encoding:'UTF-8'});
 console.log(fileContents);
